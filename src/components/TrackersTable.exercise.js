ad6cd3aab17c928f0fcf7e90b8114d5c86171bce
cellouRetailShake <cellu@retailshake.com>
2024-05-05 15:46:08 +0200

    feat: 🎸 add category split

--- src/components/TrackersTable.exercise.js
+++ src/components/TrackersTable.exercise.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import './Trackers.css'
-// 🐶 importe le helper groupBy
-// 🤖 import {groupBy} from '../helper'
+import {groupBy, diffTime} from '../helper'
 
 const TrackerCategory = ({category}) => {
   return (
@@ -14,34 +13,30 @@ const TrackerCategory = ({category}) => {
 }
 
 const TrackerRow = ({tracker}) => {
+  const duration = diffTime(tracker?.starttime, tracker?.endtime )
   return (
     <tr>
       <td>{tracker.name}</td>
       <td>{tracker.starttime}</td>
       <td>{tracker.endtime}</td>
-      <td>2j 3h 45min 18 sec </td>
+      <td>{duration}</td>
     </tr>
   )
 }
 
 const TrackersTable = ({trackers}) => {
-  // 🐶 créé un array 'rows' qui contindra nos lignes à afficher
-  // 🐶 créé un string 'lastCategory' qui contindra la dernière
-  // catégorie traitée :  (sera utile plus tard)
-
-  // 🐶 appelle 'groupBy'
-  const trackersParCategory = []
-  // 🤖 const trackersParCategory = groupBy(trackers, 'category')
-
+  const rows = []
+  let lastCategory =''
+  const trackersParCategory = groupBy(trackers, 'category')
+  
   Object.keys(trackersParCategory).forEach(category => {
     trackersParCategory[category].forEach(tracker => {
-      // 🐶 La première fois on ajoute dans 'rows' ligne catégorie 🤖 `rows.push(<TrackerCategory`
-      // pour savoir si c'est la première fois que l'on rencontre une catégorie, on se base dur 'lastCategory'
-      // 🤖 if (tracker.category !== lastCategory) {
-      // 🐶 ajoute le tracker dans rows 🤖 `rows.push(<TrackerRow`
-      // n'oublie pas les key sur les composants
-      // 🐶 met à jour la variable 'lastCategory' avec  'tracker.category' pour que l'on ajoute
-      // pas une deuxieme ligne <TrackerCategory> pour rien
+      if (tracker.category !== lastCategory) {
+        rows.push(<TrackerCategory key={category} category={category}/>)
+
+      }
+      rows.push(<TrackerRow key={tracker.id} tracker={tracker}/>)
+      lastCategory = tracker.category
     })
   })
   return (
@@ -58,10 +53,7 @@ const TrackersTable = ({trackers}) => {
             </tr>
           </thead>
           <tbody>
-            {/* ⛏️ supprime le `trackers.map...` et remplace par {rows}   */}
-            {trackers.map(tracker => (
-              <TrackerRow tracker={tracker} />
-            ))}
+            {rows}
           </tbody>
         </table>
       </div>
