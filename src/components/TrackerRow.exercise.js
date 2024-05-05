a74ce67e2885a0b6eab2b054f6bdc25684ed6cf3
cellouRetailShake <cellu@retailshake.com>
2024-05-05 17:12:49 +0200

    feat: 🎸 add reactive duration

--- src/components/TrackerRow.exercise.js
+++ src/components/TrackerRow.exercise.js
@@ -3,29 +3,26 @@ import * as React from 'react'
 import {diffTime} from '../helper'
 
 const TrackerRow = ({tracker, selectedId, onSelected}) => {
-  
-  // 🐶 créé un variable 'starttime' qui contient un string au format 'toLocaleString'
-  // 🐶 créé un variable 'endtime' qui contient un string au format 'toLocaleString'
-  // 🐶 utilise une ternaire pour afficher la date de fin ou 'en cours ...'
-  // 📑 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+  const startTime = new Date(tracker?.starttime).toLocaleString()
+  const endTime = tracker?.endtime
+    ? new Date(tracker?.endtime).toLocaleString()
+    : 'en cours ...'
 
-  // 🐶 nous allons maintenant vouloir refraichir automatiquement la durée toutes les secondes
-  // pour cela il faut changer 'duration' en state.
-  // ⛏️ Remplace duration en state
-  const duration = diffTime(tracker?.starttime, tracker?.endtime)
+  const [duration, setDuration] = React.useState(
+    diffTime(tracker?.starttime, tracker?.endtime),
+  )
 
-  // 🐶 L'idée et d'appeler une fonction 'refresh' toutes les secondes grace à 'setTimeOut'
-  // 📑 https://www.w3schools.com/jsref/met_win_settimeout.asp
-  // Utilise useEffect pour declarer la fonction 'refresh' et le 'timer'
+
   React.useEffect(() => {
-    // 🐶 crée une fonction 'refresh' qui met à jour le state 'duration'
+    const refresh = () => {
+      setDuration(diffTime(tracker?.starttime, tracker?.endtime))
+    }
+    const timerID = setTimeout(() => refresh(), 1000)
+    return () => {
+      clearTimeout(timerID)
+    }
 
-    // 🐶 utilise setTimeout pour appler 'refresh' toutes les secondes
-    // 🤖 const timerID = setTimeout(() => refresh(), 1000)
-    // n'oulie pas le cleanup de useEffect en retournant 'clearTimeout(timerID)'
-
-    // 🐶 pense aux dependances
-  }, [])
+  }, [duration, tracker?.endtime, tracker?.starttime])
 
   const handleClick = e => {
     onSelected(tracker)
@@ -34,10 +31,9 @@ const TrackerRow = ({tracker, selectedId, onSelected}) => {
   return (
     <tr className={selected} onClick={handleClick}>
       <td>{tracker.name}</td>
-      {/* 🐶 remplace les dates */}
-      <td>{tracker.starttime}</td>
-      <td>{tracker.endtime}</td>
-      <td>{duration}</td>
+      <td>{startTime}</td>
+      <td>{endTime ? endTime : 'en cours ...'}</td>
+      <td>{duration}</td> 
     </tr>
   )
 }
